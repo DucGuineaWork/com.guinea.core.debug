4835846fd0a40c6ae9987e7358ce4de9d90ad36c
Pham Duc <ducpt.guinea.work@gmail.com>
2024-07-21 22:58:25 +0700

    * Fix targetFrameRate

--- Runtime/Guinea/Core/Debug/Setting.cs
+++ Runtime/Guinea/Core/Debug/Setting.cs
@@ -33,7 +33,7 @@ namespace Guinea.Core.Debug
 
         public void OnFPSDropDownValueChanged(int value)
         {
-            QualitySettings.SetQualityLevel( m_fpsDropDown.value >=0 &&m_fpsDropDown.value< s_fpsMapper.Length ? s_fpsMapper[m_fpsDropDown.value] : -1, true);
+            Application.targetFrameRate = m_fpsDropDown.value >=0 &&m_fpsDropDown.value< s_fpsMapper.Length ? s_fpsMapper[m_fpsDropDown.value] : -1, true);
         }
 
         public void Toggle()
