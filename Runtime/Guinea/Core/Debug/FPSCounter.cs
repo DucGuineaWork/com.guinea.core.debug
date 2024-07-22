965e1f01c20ab7fff7d925cae1662fca8119a916
Pham Duc <ducpt@valofe.com>
2024-07-22 10:08:12 +0700

    + Add FPSCounter

---
+++ Runtime/Guinea/Core/Debug/FPSCounter.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Guinea.Core.Debug
+{
+    public class FPSCounter : MonoBehaviour 
+    {
+        [SerializeField]Text m_fpsText;
+        [SerializeField]Text m_fpsMinText;
+        [SerializeField]Text m_fpsMaxText;
+        const float fpsMeasurePeriod = 0.5f;
+        private int m_FpsAccumulator = 0;
+        private float m_FpsNextPeriod = 0;
+        private int m_CurrentFps;
+        private int m_minFps = Int32.MaxValue;
+        private int m_maxFps = Int32.MinValue;
+
+        private void Start() 
+        {
+            m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
+        }
+
+        private void Update() 
+        {
+            m_FpsAccumulator++;
+            if (Time.realtimeSinceStartup> m_FpsNextPeriod) 
+            {
+                m_CurrentFps = (int)(m_FpsAccumulator / fpsMeasurePeriod);
+                m_minFps = Mathf.Min(m_minFps, m_CurrentFps);
+                m_maxFps = Mathf.Max(m_maxFps, m_CurrentFps);
+                m_FpsAccumulator = 0;
+                m_FpsNextPeriod += fpsMeasurePeriod;
+                m_fpsText.text = $"FPS: {m_CurrentFps}";
+                m_fpsMinText.text = $"Min: {m_minFps}";
+                m_fpsMaxText.text = $"Max: {m_maxFps}";
+            }
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            if(!pauseStatus)
+            {
+                m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
+            }
+        }
+    }
+}
